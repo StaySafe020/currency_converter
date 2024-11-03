262f0731bb4ac30764fb98e0c4dad4a3ce554826
jambong-ralpher <jambongralpher@gmail.com>
2024-11-03 23:25:55 +0100

    set up a WebSocket server

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
-import asyncio
-import websockets
 
-async def consumer_handler(websocket, path):
-    while True:
-        # Fetch exchange rates from the API
-        # ... (code to fetch rates using requests or other libraries)
-        await websocket.send(json.dumps(exchange_rates))
-        await asyncio.sleep(60)  # Update every 60 seconds
 
-async def main():
-    async with websockets.serve(consumer_handler, "localhost", 8765):
-        await asyncio.Future()  # Run forever
+from flask import Flask, render_template
 
-asyncio.run(main())
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
